0f640d19d7866772f5cb6dadf97ce790b7699cee
CEREBRO <33547482+priyesh2@users.noreply.github.com>
2023-03-07 19:56:23 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Define the version of Terraform to install
-TERRAFORM_VERSION=1.0.8
-
-# Define the download URL for the Terraform binary
-TERRAFORM_DOWNLOAD_URL=https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
 # Define the directory where Terraform will be installed
 TERRAFORM_INSTALL_DIR=/usr/local/bin
 
@@ -15,12 +9,14 @@ if ! command -v unzip >/dev/null 2>&1; then
     sudo apt-get update && sudo apt-get install -y unzip
 fi
 
-# Download the Terraform binary
-echo "Downloading Terraform ${TERRAFORM_VERSION}..."
+# Download the latest version of Terraform
+echo "Downloading latest version of Terraform..."
+LATEST_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
+TERRAFORM_DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${LATEST_VERSION}/terraform_${LATEST_VERSION}_linux_amd64.zip"
 sudo curl -Lo /tmp/terraform.zip $TERRAFORM_DOWNLOAD_URL
 
 # Unzip the Terraform binary and move it to the install directory
-echo "Installing Terraform ${TERRAFORM_VERSION}..."
+echo "Installing Terraform ${LATEST_VERSION}..."
 sudo unzip -o /tmp/terraform.zip -d $TERRAFORM_INSTALL_DIR
 sudo chmod +x ${TERRAFORM_INSTALL_DIR}/terraform
 
@@ -29,3 +25,4 @@ echo "Verifying Terraform installation..."
 terraform version
 
 echo "Done."
+
